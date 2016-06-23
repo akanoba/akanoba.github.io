6542504832322e58079144fd11b171ebfd6db0f0
yuuri <yuuri.sakagami1997@gmail.com>
2016-06-23 20:41:20 +0900

    test

--- function.js
+++ function.js
@@ -1,5 +1,24 @@
-﻿function bootstrap() {
+﻿var loadMenuStatus = 0;
+
+function bootstrap() {
 
 
+
+}
+
+function loadMenu(){
+
+    if(loadMenuStatus == 0){
+        $('#head-contents-container').css('display','block').animate({
+            opacity:0.8
+        },100)
+
+        loadMenuStatus =1;
+    }else if(loadMenuStatus==1){
+        $('#head-contents-container').animate({
+            opacity:0
+        },100,function(){$('#head-contents-container').css('display','none')});
+        loadMenuStatus=0;
+    }
 
 }
