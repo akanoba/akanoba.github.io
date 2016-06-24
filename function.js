82b5824f066d775d952858ca775a9828a35c1ee2
yuuri <yuuri.sakagami1997@gmail.com>
2016-06-24 23:26:29 +0900

    add load button and something

--- function.js
+++ function.js
@@ -1,23 +1,70 @@
 ﻿var loadMenuStatus = 0;
 
 function bootstrap() {
+    loadBtn();
+}
 
+function scroll(posId){
+    var e = $(posId).offset().top;
 
+    $('html,body').animate({
+        scrollTop:e-80
+    });
 
+}
+
+function loadBtn(){
+
+       setTimeout(function(){
+           if (scrolled == 0 && loadMenuStatus ==0){
+               $('#load_button').css('display','block')
+               .animate({
+                   opacity:1
+               },1000).click(function(e){
+                   loadClicked();
+               });
+       }
+    },7000);
+}
+
+function loadClicked(){
+
+    scroll('#story');
+
+    $('#load_button').animate({
+        opacity:0
+    },function(){$('#load_button').css('display','none')});
 }
 
 function loadMenu(){
 
+    if(scrolled == 0){
+        $('#load_button').animate({
+            opacity:0
+        })
+            .css('display','none');
+    }
+
     if(loadMenuStatus == 0){
         $('#head-contents-container').css('display','block').animate({
             opacity:0.8
-        },100)
+        },100);
+
+        $('#home-overlay-menu').css('display','block').animate({
+            opacity:1
+        },100);
+        
 
         loadMenuStatus =1;
     }else if(loadMenuStatus==1){
         $('#head-contents-container').animate({
             opacity:0
         },100,function(){$('#head-contents-container').css('display','none')});
+        
+        $('#home-overlay-menu').animate({
+            opacity:0
+        },100,function(){$('#home-overlay-container').css('display','none')});
+        
         loadMenuStatus=0;
     }
 
