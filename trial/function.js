7327dffb06f3ae76aa84cc3aa990505aa9428ff5
yuuri <yuuri.sakagami1997@gmail.com>
2016-12-29 03:54:46 +0900

    体験版Webページ更新

---
+++ trial/function.js
@@ -0,0 +1,8 @@
+function fullscreen(){
+    var game = document.getElementById('game');
+
+    if(document.fullscreenEnabled)game.requestFullscreen();
+    if(document.webkitFullscreenEnabled)game.webkitRequestFullscreen();
+    if(document.msFullscreenEnabled)game.msRequestFullscreen();
+    if(document.mozFullScreenEnabled)game.mozRequestFullScreen();
+}
