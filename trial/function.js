8f15a52393fcb990e01cc14ad89cf3ae81c2acd1
yuuri <yuuri.sakagami1997@gmail.com>
2016-12-29 04:24:09 +0900

    ああああああああああああああ

--- trial/function.js
+++ trial/function.js
@@ -1,6 +1,8 @@
 function fullscreen(){
     var game = document.getElementById('game');
 
+    document.getElementById('test').innerHTML = 'aaaaaaaaaaaa';
+
     if(document.fullscreenEnabled)game.requestFullscreen();
     if(document.webkitFullscreenEnabled)game.webkitRequestFullscreen();
     if(document.msFullscreenEnabled)game.msRequestFullscreen();
